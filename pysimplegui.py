216e158bb046a7b62520dfced844336cc22f6d23
kyewonha <you22th@naver.com>
2024-02-09 21:45:34 +0900

    버튼 기능 추가 및 todos.txt에 관련내용 추가

--- pysimplegui.py
+++ pysimplegui.py
@@ -1,9 +1,26 @@
 import PySimpleGUI as sg
+import functions
+
 
 label = sg.Text("Type in a to-do")
-input_box =  sg.InputText(tooltip = "enter todo")
+input_box =  sg.InputText(tooltip = "enter todo", key='todo')
 add_button = sg.Button("Add")
 
-window = sg.Window("My to DO App", layout= [[label],[input_box, add_button]])
-window.read()
+window = sg.Window("My to DO App", layout= [[label],[input_box, add_button]], font=('Helvetica', 20))
+
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todo = values["todo"] + "\n"
+            todos.append(new_todo)
+            functions.write_todos(todos)
+
+        case sg.WIN_CLOSED:
+            # gui끄면 break
+            break
+
 window.close()
